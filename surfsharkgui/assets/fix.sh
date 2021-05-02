31c1b410f484655dcc38d0e7574331fdbc87a895
Talhoid-Coder <75804781+Talhoid-Coder@users.noreply.github.com>
2021-05-02 13:41:16 -0400

    Create fix.sh

---
+++ surfsharkgui/assets/fix.sh
@@ -0,0 +1,3 @@
+ip rule add from $(ifconfig eth0 | grep -oP "\binet\s+\K\S+" --color=never) table 128
+ip route add table 128 to $(whois $(ifconfig eth0 | grep -oP "\binet\s+\K\S+" --color=never) | grep -oP "\bCIDR:\s+\K\S+" --color=never) dev eth0
+ip route add table 128 default via $(ip route show | grep -oP "\bdefault via\s+\K\S+" --color=never)
